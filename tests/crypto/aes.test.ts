a0ca66438e03202e0d67608421e921ef62127666
raphckrman <41128238+raphckrman@users.noreply.github.com>
2025-10-01 19:31:31 +0200

    tests: remove useless benchmarks tests

--- tests/crypto/aes.test.ts
+++ tests/crypto/aes.test.ts
@@ -4,33 +4,6 @@ import {randomBytes} from "@noble/hashes/utils.js";
 
 const aes = new AES();
 
-test("should encrypt 1k values under 7ms", () => {
-    let i = 0;
-    while (i < 1e3) {
-        aes.encrypt(String(i));
-        i++
-    }
-    expect(Bun.nanoseconds()).toBeLessThan(70000000);
-});
-
-test("should encrypt 10k values under 150ms", () => {
-    let i = 0;
-    while (i < 1e4) {
-        aes.encrypt(String(i));
-        i++
-    }
-    expect(Bun.nanoseconds()).toBeLessThan(150000000);
-});
-
-test("should encrypt 100k values under 4s", () => {
-    let i = 0;
-    while (i < 1e5) {
-        aes.encrypt(String(i));
-        i++
-    }
-    expect(Bun.nanoseconds()).toBeLessThan(4e9);
-});
-
 test("should return correct ciphertext for default key/IV", () => {
     const result = aes.encrypt("1")
     expect(result).toBe("3fa959b13967e0ef176069e01e23c8d7");
