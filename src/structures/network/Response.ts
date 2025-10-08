8259ade61c7eb085f8beefff2c7971410fda514a
raphckrman <41128238+raphckrman@users.noreply.github.com>
2025-10-08 17:27:12 +0200

    feat(network): automatically parse the data in response

--- src/structures/network/Response.ts
+++ src/structures/network/Response.ts
@@ -1,9 +1,13 @@
+import { Parser } from "../parsing/Parser";
+
 export class Response<T> {
   constructor(
     public headers: Record<string, string>,
     public status: number,
     public data: T
-  ){}
+  ){
+    this.data = Parser.parse(data);
+  }
   
   public header(key: string): string | undefined {
     return this.headers[key.toLowerCase()];
