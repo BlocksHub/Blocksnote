5cc2b1b3a4ecb9a48c3f7afd55e34210f0e9ffdf
raphckrman <41128238+raphckrman@users.noreply.github.com>
2025-10-08 17:26:21 +0200

    tests: add unit tests for requests

--- tests/network/request.test.ts
+++ tests/network/request.test.ts
@@ -1,9 +1,14 @@
 import { test, expect } from "bun:test";
 import { Request } from "../../src/structures/network/Request.ts";
+import { RESTManager } from "../../src/structures/network/RESTManager.ts";
+
+let count = 0
+const manager = new RESTManager();
 
 const server = Bun.serve({
     fetch(req) {
-        return new Response(JSON.stringify(req.headers), { status: 200 });
+        count++;
+        return new Response(JSON.stringify(req.headers), { status: 200, headers: {"X-Request-Count": String(count)} });
     }
 })
 
@@ -13,7 +18,7 @@ test("should update specific header", async () => {
         .setEndpoint("localhost:" + server.port);
 
     const response = await request.send<Record<string, string>>()
-    expect(response["x-custom-header"]).toBe("@Blockshub/Blocksnote");
+    expect(response.data["x-custom-header"]).toBe("@Blockshub/Blocksnote");
 })
 
 test("should update multiple headers", async () => {
@@ -25,8 +30,8 @@ test("should update multiple headers", async () => {
         .setEndpoint("localhost:" + server.port);
 
     const response = await request.send<Record<string, string>>()
-    expect(response["x-custom-header"]).toBe("@Blockshub/Blocksnote");
-    expect(response["x-readme"]).toBe("Star the repository");
+    expect(response.data["x-custom-header"]).toBe("@Blockshub/Blocksnote");
+    expect(response.data["x-readme"]).toBe("Star the repository");
 })
 
 test("should update endpoint", async () => {
@@ -34,4 +39,33 @@ test("should update endpoint", async () => {
         .setEndpoint("localhost:" + server.port);
 
     expect(request.endpoint).toBe("localhost:" + server.port)
+})
+
+test("should update method", async () => {
+    const request = new Request()
+        .setMethod("POST");
+
+    expect(request.method).toBe("POST")
+})
+
+test("should update payload", async () => {
+    const request = new Request()
+        .setPayload(245934, "Blocksnote", "UpdatePayload", {});
+
+    expect(JSON.stringify(request.payload)).toBe("{\"session\":245934,\"no\":\"Blocksnote\",\"id\":\"UpdatePayload\",\"dataSec\":{}}")
+})
+
+test("should serialize requests", async () => {
+    const first = new Request().setEndpoint("localhost:" + server.port);
+    const second = new Request().setEndpoint("localhost:" + server.port);
+
+    const firstCall = await manager.enqueueRequest<Record<string, string>>(first)
+    const secondCall = await manager.enqueueRequest<Record<string, string>>(second)
+
+    expect((Number(firstCall.header("X-Request-Count")) === 3 && Number(secondCall.header("X-Request-Count")) === 4)).toBe(true)
+})
+
+test("should throw error on missing parameters", async () => {
+    const request = new Request();
+    expect(async () => await request.send()).toThrow();
 })
