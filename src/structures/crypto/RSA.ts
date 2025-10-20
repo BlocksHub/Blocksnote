c1acc10395b5905788be2cb46077c9b41eefce48
raphckrman <41128238+raphckrman@users.noreply.github.com>
2025-10-20 22:51:16 +0200

    feat(crypto): add RSA encryption

---
+++ src/structures/crypto/RSA.ts
@@ -0,0 +1,12 @@
+import { PKCS1_KEM, type PublicKey } from "micro-rsa-dsa-dh/rsa.js";
+import { EXPONENT_1024, MODULUS_1024 } from "../../utils/constants";
+import { utf8ToBytes } from "@noble/hashes/utils.js";
+
+export class RSA {
+  public static publicKey1024: PublicKey = { e: BigInt("0x" + EXPONENT_1024), n: BigInt("0x" + MODULUS_1024) };
+
+  public static encrypt1024(str: string | Uint8Array) {
+    const pkcs = PKCS1_KEM;
+    return pkcs.encrypt(RSA.publicKey1024, (str instanceof Uint8Array) ? str : utf8ToBytes(str));
+  }
+}
