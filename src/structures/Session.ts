d547587759df0dbba11a3052c268cb88689a4152
raphckrman <41128238+raphckrman@users.noreply.github.com>
2025-10-28 03:56:28 +0100

    feat(session): add encryption + compression properties

--- src/structures/Session.ts
+++ src/structures/Session.ts
@@ -1,9 +1,8 @@
-import { fetchInstanceInformations } from "../routes/instance";
 import { type Workspace } from "../types/authflow";
 import { BYPASS_ID } from "../utils/constants";
 import { AES } from "./crypto/AES";
 import { AuthenticationError } from "./errors/AuthenticationError";
-import type { Instance } from "./Instance";
+import { Instance } from "./Instance";
 import { Request } from "./network/Request";
 import { RequestManager } from "./network/RequestManager";
 
@@ -17,7 +16,9 @@ export class Session {
   constructor(
     public id: string,
     public source: string,
-    public workspace: Workspace
+    public workspace: Workspace,
+    public useCompression: boolean = false,
+    public useEncryption: boolean = false
   ){
     if (this.source.startsWith("https")) {
       this.useHttps = true;
@@ -33,12 +34,15 @@ export class Session {
     }
 
     const sessionId = response.data.match(/h:([^,)}]+)/)?.[1];
+    const hasCrA: boolean = response.data.match(/CrA/gm) ? true : false;
+    const hasCoA: boolean = response.data.match(/CoA/gm) ? true : false;
+
     if (!sessionId) {
       throw new AuthenticationError("Unable to create a session for this instance");
     }
 
-    const session = new Session(sessionId, source, workspace);
-    const instance = await fetchInstanceInformations(session)
+    const session = new Session(sessionId, source, workspace, hasCoA, hasCrA);
+    const instance = await Instance.load(session)
     session.instance = instance
     return  session
   }
