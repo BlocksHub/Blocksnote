2c97c4a2c05a18fea88bf364619ec2ccffe74f0f
raphckrman <41128238+raphckrman@users.noreply.github.com>
2025-11-09 02:30:49 +0100

    fix(session): add http parameter to ensure that encryption is correctly managed

--- src/structures/Session.ts
+++ src/structures/Session.ts
@@ -1,4 +1,4 @@
-import { type Workspace } from "../types/authflow";
+import { type Workspace } from "../types/authentication";
 import { BYPASS_ID } from "../utils/constants";
 import { AES } from "./crypto/AES";
 import { AuthenticationError } from "./errors/AuthenticationError";
@@ -11,19 +11,16 @@ export class Session {
   public aes = new AES();
 
   public instance?: Instance;
-  public useHttps: boolean = false;
+  
 
   constructor(
     public id: string,
     public source: string,
     public workspace: Workspace,
     public useCompression: boolean = false,
-    public useEncryption: boolean = false
-  ){
-    if (this.source.startsWith("https")) {
-      this.useHttps = true;
-    }
-  }
+    public useEncryption: boolean = false,
+    public useHttps: boolean = false
+  ){}
 
   public static async createSession(source: string, workspace: Workspace) {
     const endpoint = `${source}${workspace.url}?fd=1&bydlg=${BYPASS_ID}`;
@@ -36,12 +33,13 @@ export class Session {
     const sessionId = response.data.match(/h:([^,)}]+)/)?.[1];
     const hasCrA: boolean = response.data.match(/CrA/gm) ? true : false;
     const hasCoA: boolean = response.data.match(/CoA/gm) ? true : false;
+    const useHttps: boolean = !(/http\s*:\s*true/.test(response.data));
 
     if (!sessionId) {
       throw new AuthenticationError("Unable to create a session for this instance");
     }
 
-    const session = new Session(sessionId, source, workspace, hasCoA, hasCrA);
+    const session = new Session(sessionId, source, workspace, hasCoA, hasCrA, useHttps);
     const instance = await Instance.load(session)
     session.instance = instance
     return  session
