f5b65b122391863f472afa910b5a753b4be4af9c
raphckrman <41128238+raphckrman@users.noreply.github.com>
2025-12-07 00:17:12 +0100

    feat(user): add types for UserSettings

---
+++ src/types/user.ts
@@ -0,0 +1,54 @@
+import type { Attachment } from "../structures/Attachment"
+import type { Period } from "./instance"
+
+export type Class = {
+  current: boolean,
+  withGrades: boolean,
+  withFiliere: boolean,
+  label: string
+}
+
+export type Tab = {
+  periods?: Period[]
+}
+
+export type HarassmentPolicy = {
+  referents: HarassmentReferent[];
+  supportNumber?: PhoneNumber;
+}
+
+export type HarassmentReferent = {
+  canChatWith: boolean;
+  name: string;
+  role?: string;
+}
+
+export type Establishment = {
+  name: string;
+  id: string;
+  logo: string;
+  website: string;
+  location: Coordinates;
+  mails: Mail[];
+  phoneNumbers: PhoneNumber[];
+  rules?: Attachment;
+  charter?: Attachment;
+  harassmentPolicy: HarassmentPolicy;
+}
+
+export type Mail = {
+  mail: string;
+  label: string;
+}
+
+export type PhoneNumber = {
+  number: string;
+  label: string;
+}
+
+export type Coordinates = {
+  address: string;
+  postalCode: string;
+  city: string;
+  province: string;
+}
