dd45b80731df9004c0e1a19696e600850e1b40b2
raphckrman <41128238+raphckrman@users.noreply.github.com>
2025-12-07 00:18:35 +0100

    chore: clean code

--- src/structures/Attachment.ts
+++ src/structures/Attachment.ts
@@ -1,3 +1,4 @@
+import type { FileTypeValue } from "../types/attachments";
 import { Session } from "./Session";
 
 export class Attachment {
@@ -19,17 +20,3 @@ export class Attachment {
         );
     }
 }
-
-export const FileType = {
-    NONE: "Aucun",
-    TEST_SUBJECT: "EvaluationSujet",
-    TEST_CORRECTION: "EvaluationCorrige",
-    ASSIGNMENT_SUBJECT: "DevoirSujet",
-    ASSIGNMENT_CORRECTION: "DevoirCorrige",
-    HOMEWORK_SUBMISSION: "TAFRenduEleve",
-    HOMEWORK_CORRECTION: "TAFCorrigeRenduEleve",
-    SCHOOL_RULES: "EtablissementReglement",
-    SCHOOL_CHARTER: "EtablissementCharte",
-    SIGNUP_ATTACHMENT: "DocJointInscription"
-}
-export type FileTypeValue = typeof FileType[keyof typeof FileType];
