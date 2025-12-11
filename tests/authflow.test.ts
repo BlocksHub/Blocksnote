bf177746c7ae664b08cb83cf1785f819af03ff3e
raphckrman <41128238+raphckrman@users.noreply.github.com>
2025-12-11 01:25:51 +0100

    chore: update tests

--- tests/authflow.test.ts
+++ tests/authflow.test.ts
@@ -1,5 +1,5 @@
 import { test, expect } from "bun:test";
-import { Authenticator } from "../src/structures/Authenticator";
+import { Instance } from "../src/structures/Instance";
 
 test.each([
   "https://demo.index-education.net/pronote/eleve.html",
@@ -20,15 +20,15 @@ test.each([
   "   https://demo.index-education.net/pronote/eleve.html   ",
   "//demo.index-education.net/pronote/eleve.html"
 ])("should clean %s", (input) => {
-  expect(Authenticator.cleanUrl(input)).toBeOneOf(["https://demo.index-education.net/pronote/", "http://demo.index-education.net/pronote/", "https://demo.index-education.net/"])
+  expect(Instance.cleanUrl(input)).toBeOneOf(["https://demo.index-education.net/pronote/", "http://demo.index-education.net/pronote/", "https://demo.index-education.net/"])
 })
 
-const instance = await Authenticator.createFromURL("demo.index-education.net/pronote")
+const instance = await Instance.createFromURL("demo.index-education.net/pronote")
 
 test("should initialize the auth flow", async () => {
   expect(
     instance.source === "https://demo.index-education.net/pronote/"
-    && instance.availableWorkspaces.every(item => item.delegated === false)
+    && instance.workspaces.every(item => item.delegated === false)
     && instance.version.length > 0
     && !instance.cas
   ).toBe(true)
