1727de7ca2d2970945787eb4559513583651fbeb
raphckrman <41128238+raphckrman@users.noreply.github.com>
2025-12-28 21:11:08 +0100

    feat(user): add childrens for parent, and permissions

--- src/structures/ParametresUtilisateurs/Student.ts
+++ src/structures/ParametresUtilisateurs/Student.ts
@@ -1,8 +1,9 @@
 import { CommonUserSettings } from "./Common";
 import { Response } from "../network/Response";
-import type { Base64, EleveParametresUtilisateurResponse, PronoteClasse, StudentClass } from "./Common";
 import type { Session } from "../Session";
 import type { Settings } from "../Settings";
+import type { EleveParametresUtilisateurResponse, PronoteClasse } from "../../types/responses/user";
+import type { Base64, StudentClass, StudentPermissions } from "../../types/user";
 
 export class StudentUserSettings extends CommonUserSettings<EleveParametresUtilisateurResponse> {
   constructor(
@@ -22,6 +23,19 @@ export class StudentUserSettings extends CommonUserSettings<EleveParametresUtili
       label: item?.label ?? ""
     }
   }
+  
+  public override get permissions(): StudentPermissions {
+    const common = super.permissions
+    const authorizations = this.raw.data.autorisations
+
+    return {
+      ...common,
+      size: {
+        ...common.sizes,
+        studentHomeworkMaxSize: authorizations.tailleMaxRenduTafEleve
+      }
+    }
+  }
 
   public get groups(): string[] {
     return this.ressource.listeGroupes.map(group => group.label);
@@ -37,8 +51,7 @@ export class StudentUserSettings extends CommonUserSettings<EleveParametresUtili
   }
 
   public get profilePicture(): Base64<'png'> | undefined {
-    const ressource = this.raw.data.ressource;
-    const key = ressource.photoBase64;
+    const key = this.ressource.photoBase64;
     const file = key !== undefined ? this.raw.ressources?.fichiers?.[key] : undefined;
 
     return ["data:image/png;base64", file].join(",") as Base64<'png'>
